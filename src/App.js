9419e30c36dcd5ddfd9ef14563db73dbc3c6704b
sbutextbooks <stonybrook.textbooks@gmail.com>
2020-07-09 23:58:55 -0400

    revert back to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import { HashRouter, Route, Link } from "react-router-dom";
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Route, Link } from "react-router-dom";
 import TextbookList from './components/TextbookList';
 import TextbookUploadForm from './components/TextbookUploadForm';
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <HashRouter basename='/'>
       <div className="App">
         <Route exact path="/" component={TextbookList} />
         <Route path="/upload" component={TextbookUploadForm} />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
